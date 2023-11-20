db74a8703dcd288a58202d4cf5109f437cfd348f
Kevs <79332227+kevsmartini@users.noreply.github.com>
2023-11-20 18:11:36 +0000

    playRound function created to play a round of the game

--- javascript.js
+++ javascript.js
@@ -9,4 +9,32 @@ const getComputerChoice = () => {
 }
 
 
-console.log(getComputerChoice())
+//console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection) {
+
+    //const lowerCase = playerSelection ? playerSelection.toLowerCase() : "";
+    const lowerCase = playerSelection.toLowerCase();
+
+      
+    if(lowerCase === 'rock' && computerSelection === 'Paper'){
+    //   console.log('You Lose! Paper beats Rock')
+      return 'You Lose! Paper beats Rock'
+    }else if(lowerCase === 'rock' && computerSelection === 'Scissors'){
+    //   console.log('You Win! Rock beats Scissors')
+        return 'You Win! Rock beats Scissors'
+    }else{
+    //   console.log('Try again it is a draw')
+        return 'Try again it is a draw'
+      
+    }
+
+    
+}  
+
+const playerSelection = "rocK";
+const computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+
+console.log(playRound(playerSelection,computerSelection));
